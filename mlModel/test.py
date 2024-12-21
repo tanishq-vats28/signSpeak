6eb789424ef7ad9528bc1f7e190780ec0a93acd6
Tanishq Vats <tanishqvats620@gmail.com>
2024-12-21 19:19:37 +0530

    third commit

--- mlModel/test.py
+++ mlModel/test.py
@@ -62,6 +62,7 @@ def detect():
 
     return jsonify({"text": "No hand detected"})
 
-app.run(host='0.0.0.0', port=5000, debug=True) 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 
